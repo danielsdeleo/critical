0ac33cbd6bc879991444b04935e2e4423fcde0cf
Dan DeLeo <danielsdeleo@mac.com>
2010-03-25 23:08:24 -0700

    update examples for syntax changes

--- examples/config/critical_config.rb
+++ examples/config/critical_config.rb
@@ -1,5 +1,10 @@
 require 'rubygems'
 
+# Monkey patches are opt-in ATM, probably will be opt out if I keep them around
+require 'critical/monkey_patch/array'
+require 'critical/monkey_patch/string'
+
+
 Critical.configure do |critical|
   root = File.dirname(__FILE__) + '/../'
   critical.require root + "metrics/"
--- examples/metrics/rest.rb
+++ examples/metrics/rest.rb
@@ -3,15 +3,15 @@ require "restclient"
 
 Metric(:http_get) do |http_get|
   http_get.monitors :url
-  http_get.collects { RestClients.get(url) }
+  http_get.collects { RestClient.get(url) }
   http_get.reports(:status => :integer) {result.code}
   http_get.reports(:content) { result.body }
 end
 
 Monitor(:web) do
-  http_get("http://opscode.com/") do
-    status.is equal_to 200
+  http_get("http://opscode.com/") do |request|
+    request.status.is equal_to 200
     # Hrm, maybe I need to give in and use #should...
-    #content.at_selector('css-selector').matches "something"
+    # request.content.at_selector('css-selector').matches "something"
   end
 end
