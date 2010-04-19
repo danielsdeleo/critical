376f10a8da3c263be3fd4f595a0bdf8be58cf111
Dan DeLeo <danielsdeleo@mac.com>
2010-04-18 17:22:49 -0700

    update examples and notes

--- examples/metrics/integration_monitor.rb
+++ examples/metrics/integration_monitor.rb
@@ -1,21 +1,38 @@
+require 'mechanize'
+require 'webrat'
+
+Metric(:browser) do
+  
+  def after_initialize
+    @mechanize = ::Mechanize.new
+  end
+
+  def visit(url)
+    @base_url = url
+  end
+end
+
+
 # Namespacing: should be namespaceable via Monitor() like everything else;
 # should show up as $namespace/story("story title")
 # steps show up as $namespace/story("story title")/step("step title") <= assuming there's value in breaking down steps
-Story("Signup for the Opscode Platform") do |s|
+Story("Signup for my website") do |s|
 	# step should be aliased as #given, #when and #then
   s.step("I complete the signup form") do
     # In here, we're in the same context as in a Monitor() block.
-    # the #browser behavior below can be implemented as a Metric()
-    # if we switch Metric to using class eval...
-    
+
     # browser should be a wrapper around mechanize.
     # browser should take an options hash in the constructor
-    # for various things like content accept headers and the like.
-    browser do |b|
-      b.visit("http://cookbooks.opscode.com/signup").expect(:status => 200)
-      b.fill_in(:user_name => "valid-user", :password => "password", :password_confirmation => "password")
-      b.fill_in(:email_address => "testuser+slug@gmail.com")
-      b.submit("signup")
+    # for various things like content accept, base url, etc.
+    # browser can be implemented by defining methods on a
+    # browser Metric()
+    browser do |user|
+      user.visits("http://my-web20-site.example.com/signup")
+      user.fills_in(:user_name => "valid-user", :password => "password", :password_confirmation => "password")
+      user.fills_in(:email_address => "testuser+slug@gmail.com")
+      user.submits("signup")
+      # should also allow for expecting errors (404, 403, whatevr) like this (?)
+      user.expect(:status => 404).visit('/blackhole')
     end
   end
   s.step("I click confirmation link in the verification email") do
