6652c156b8976df31d695a7014a4409a67141638
Daniel DeLeo <dan@opscode.com>
2011-03-13 11:25:05 -0700

    Add config data class: parses data files.
    not yet integrated into DSL

--- lib/critical/file_loader.rb
+++ lib/critical/file_loader.rb
@@ -6,15 +6,15 @@ module Critical
   module FileLoader
     extend self
     extend Loggable
-    
+
     def load_metrics_and_monitors_in(file_or_dir)
       log.debug { "Loading source file #{file_or_dir}" }
       load_in_context(Critical::DSL::TopLevel, file_or_dir)
     end
-    
+
     def load_in_context(context_obj, file)
       assert_file_exists!(file)
-      
+
       if filename = file_full_name(file)
         load_file_in_context(context_obj, filename)
       elsif filenames = files_in_dir(file)
@@ -23,16 +23,16 @@ module Critical
         raise LoadError, "Could not load file/directory #{file}. Is it a pipe/device/alien?"
       end
     end
-    
+
     private
-    
+
     def file_full_name(file)
       filename = nil
       filename = file if File.file?(file)
       filename = file + ".rb" if File.file?(file + ".rb")
       File.expand_path(filename) if filename
     end
-    
+
     def files_in_dir(dir)
       if File.directory?(dir)
         log.debug { "Searching directory #{dir} for source files" }
@@ -41,7 +41,7 @@ module Critical
         filenames
       end
     end
-    
+
     def load_file_in_context(context_obj, filename)
       begin
         context_obj.instance_eval(IO.read(filename), filename, 1)
@@ -49,11 +49,11 @@ module Critical
         raise Critical::LoadError, "permission denied trying to access file #{file}\n Original error: #{e.message}"
       end
     end
-    
+
     def assert_file_exists!(file)
       no_such_file!(file) unless File.exist?(file) || File.exist?(file + ".rb")
     end
-    
+
     def no_such_file!(file)
       raise Critical::LoadError, "Could not load the file #{file} -- The OS says it doesn't exist"
     end
@@ -64,6 +64,7 @@ module Critical
     extend Loggable
 
     METRIC_PATH_SPEC = /(#{Regexp.escape 'critical/metrics/'}(.+))\.rb/
+    #' fix syntax highlighting in vim :(
     METRIC_REQUIRE_PATHS = {}
 
     def add_library_metric(path)
